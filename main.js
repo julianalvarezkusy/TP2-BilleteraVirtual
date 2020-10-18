4872afeaf1b738e21523eb98add67927b9405cb5
agustin <agus.maler@hotmail.com>
2020-10-18 16:28:46 -0300

    creacion de servidor con express

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hola mundo");
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use( express.json() );
+
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
+
