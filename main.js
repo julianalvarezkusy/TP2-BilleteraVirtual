2ee4fcd4d70e64ec4b6682d673adb38338cc3fa0
julianalvarezkusy <60948729+julianalvarezkusy@users.noreply.github.com>
2020-11-05 17:58:32 -0300

    servicio Export PDF2

--- main.js
+++ main.js
@@ -1,49 +1,24 @@
 const express = require("express");
 require("dotenv").config();
-// const PDF = require("./src/services/pdfconf");
-// import { pdfModule } from "./src/services/pdfModule";
-const { pdfModule } = require("./src/services/pdfModule");
-
-// // App config
-// const app = express();
-
-// // Middlewares
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// //Home
-// app.get("/", (req, res) => {
-//   return res.send("homepage");
-// });
-
-// // app.use("/api/export", require("./src/services/index"));
-
-// const text = {
-//   title: "un titulo",
-//   content: "un contenido",
-// };
-// const ruta = "./src/services/out/";
-// try {
-//   pdfModule.crearDoc();
-// } catch (error) {
-//   console.log("ERROR:" + error.message);
-// }
-
-// app.listen(process.env.PORT, () => {
-//   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
-// });
+const { pdfCreator } = require("./src/services/pdfCreator");
 
 function main() {
-  const doc = pdfModule();
+  const doc = pdfCreator();
 
   const text = {
-    title: "un titulo",
-    content: "un contenido",
+    title: "Resumen de Gastos",
+    content: [
+      { fecha: "12-12-2020", gasto: "verduleria", monto: 200 },
+      { fecha: "13-12-2020", gasto: "carniceria", monto: 300 },
+    ],
   };
 
   const ruta = "./src/services/out/";
-
-  doc.crearDoc(text.title, text.content, ruta);
+  try {
+    doc.crearDoc(text.title, text.content, ruta);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 main();
