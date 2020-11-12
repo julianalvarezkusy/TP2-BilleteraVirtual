4b45e9ca0baf7300b6e7b4b045cb08159d399904
fheili <69222288+fheili@users.noreply.github.com>
2020-11-12 19:25:12 -0300

    Agrego validación del día y variación de respuesta

--- src/casosdeuso/recordatorioCotizacion.js
+++ src/casosdeuso/recordatorioCotizacion.js
@@ -3,15 +3,29 @@
 const { crearCotizadorFactory } = require('../factories/cotizador-factory')
 const cotizador = crearCotizadorFactory().obtenerCotizador()
 
-async function cotizacion () {
+async function cotizacion() {
     const valores = await cotizador.cotizar(cotizador.DOLAR_BLUE)
     console.log("La cotización del dólar en este momento es de $" + valores.compra + " para la compra y $" + valores.venta + " para la venta.");
 }
 
-function crearCURecordatorioCotizacion (scheduler) {
+function esDiaValido(dia) {
+    let esValido = false;
+
+    if (dia > 0 && dia <= 7) {
+        esValido = true;
+    }
+
+    return esValido;
+}
+
+function crearCURecordatorioCotizacion(scheduler) {
     return {
         run: (dia) => {
-            const tarea = scheduler.programarTareaDia(dia, cotizacion); // Almaceno el job para implementar persistencia
+            if (esDiaValido(dia)) {
+                const tarea = scheduler.programarTareaDia(dia, cotizacion); // Almaceno el job para implementar persistencia
+                return tarea;
+            }
+
         }
     }
 }
--- src/ruteo/router.js
+++ src/ruteo/router.js
@@ -1,13 +1,19 @@
-const { Router } = require ("express")
+const { Router } = require("express")
 const router = Router()
 
 const { recordatorioCUFactory } = require('../factories/recordatorioCUFactory')
 const miCU = recordatorioCUFactory.getCU()
 
-router.post("/recordarcotizacion",async (req,res) => {
+router.post("/recordarcotizacion", async (req, res) => {
       console.log(req.query.dia)
-      miCU.run(req.query.dia);
-      res.send('Se fijó el recordatorio de cotización para el día ' + req.query.dia);
+
+      if (miCU.run(req.query.dia) != undefined) {
+            res.send('Se fijó el recordatorio de cotización para el día ' + req.query.dia);
+      } else {
+            res.status(400)
+            res.send('Los parámetros ingresados son inválidos')
+      }
+
 })
 
 module.exports = router;
