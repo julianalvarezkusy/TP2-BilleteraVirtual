76a709f3e1ea7472ef2a55ff2e06b5c0dd91be2e
Exequiel Fidalgo <exequielfidalgo@gmail.com>
2020-11-19 14:46:36 -0300

    Se agregaron las peticiones para la compra y venta de dolares con express.

---
+++ src/main.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const importFinancieraFactory = require('./financiera-factory')
+const financieraFactory = importFinancieraFactory.crearFinancieraFactory();
+const financiera = financieraFactory.obtenerFinanciera()
+
+const puerto = '8080'
+const app = express();
+app.use(express.json())
+
+// app.use( express.json() );
+
+const transaccionar = async (operation, amount) => {
+    const resultado = await financiera.pesificar('dolar-oficial', operation, amount)
+    const respuesta = { operacion: operation, monto: amount, pesos: resultado, codigo: 200 }
+    return respuesta
+}
+
+app.get('/venta', (req, res) => {
+    res.send('Ingrese el monto de la venta.');
+})
+
+const server = app.listen(puerto, () => {
+    console.log(`puerto: ${server.address().port}`);
+})
+
+app.get('/venta/:monto', async (req, res) => { 
+    let respuesta;
+    try {
+        respuesta = await transaccionar('venta', req.params.monto)
+    } catch (error) {
+        respuesta = { codigo: 400, mensaje: error }
+    }
+    res.json(respuesta)
+})
+
+app.get('/compra', (req, res) => {
+    res.send('Ingrese el monto de la compra.');
+})
+
+app.get('/compra/:monto', async (req, res) => {
+    let respuesta;
+    try {
+        respuesta = await transaccionar('compra', req.params.monto)
+    } catch (error) {
+        respuesta = { codigo: 400, mensaje: error }
+    }
+    res.json(respuesta)
+})
